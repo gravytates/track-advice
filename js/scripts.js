06b2659d827d186ee1d6ad2d040c32a6efa157b4
Grady L Shelton <gradyish@gmail.com>
2017-03-24 09:59:09 -0700

    write result string, set equal to result variable. hide questionaire upon submission

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,20 @@ $(document).ready(function(){
 // function ran upon submission of the questionaire
   $("#questionaire").submit(function(event) {
 
+    event.preventDefault();
+
 // set variables
     var name = $("#name input").val();
     var cost = $("#cost").val();
     var pace = $("#pace").val();
     var platform = $("#platform").val();
-    var font-back = $("#front-back").val();
+    var frontback = $("#front-back").val();
+    var result = "<strong>" + name + "</strong>, thank you for taking the time to complete our form. My infallible* advice: ";
+
+    $(".result").html(result);
+    $("#form-wrapper").hide();
 
 
-
-
-    event.preventDefault();
     alert("test");
   });
 
