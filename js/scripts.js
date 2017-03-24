bf5c44c6da30e43d82d623b83fc93d0999ae7260
Grady L Shelton <gradyish@gmail.com>
2017-03-24 16:31:15 -0700

    last test. potentially final commit

--- js/scripts.js
+++ js/scripts.js
@@ -19,41 +19,41 @@ $(document).ready(function(){
     var result = "<strong>" + name + "</strong>, thank you for taking the time to complete our form. My infallible* advice: ";
     var thanks = '<img src="img/thankyou.png" alt="thank you!">';
 
-    // long-form alerts
-    // if (!name) {
-    //   $("#name-warning").show();
-    //   exit;
-    // } else {
-    //   $("#name-warning").hide();
-    // };
-    //
-    // if (!cost) {
-    //   $("#cost-warning").show();
-    //   exit;
-    // } else {
-    //   $("#cost-warning").hide();
-    // };
-    //
-    // if (!pace) {
-    //   $("#pace-warning").show();
-    //   exit;
-    // } else {
-    //   $("#pace-warning").hide();
-    // };
-    //
-    // if (!platform) {
-    //   $("#platform-warning").show();
-    //   exit;
-    // } else {
-    //   $("#platform-warning").hide();
-    // };
-    //
-    // if (!frontback) {
-    //   $("#front-back-warning").show();
-    //   exit;
-    // } else {
-    //   $("#front-back-warning").hide();
-    // };
+    long-form alerts
+    if (!name) {
+      $("#name-warning").show();
+      exit;
+    } else {
+      $("#name-warning").hide();
+    };
+
+    if (!cost) {
+      $("#cost-warning").show();
+      exit;
+    } else {
+      $("#cost-warning").hide();
+    };
+
+    if (!pace) {
+      $("#pace-warning").show();
+      exit;
+    } else {
+      $("#pace-warning").hide();
+    };
+
+    if (!platform) {
+      $("#platform-warning").show();
+      exit;
+    } else {
+      $("#platform-warning").hide();
+    };
+
+    if (!frontback) {
+      $("#front-back-warning").show();
+      exit;
+    } else {
+      $("#front-back-warning").hide();
+    };
 
     // // page change and results
     $(".result").html(result);
