2bfe2388630a742f1b9d312d9f5e2197535ce2da
Grady L Shelton <gradyish@gmail.com>
2017-03-24 09:39:37 -0700

    set values to user input and test in browser console

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 
+// function ran upon submission of the questionaire
   $("#questionaire").submit(function(event) {
+
+// set variables
+    var name = $("#name input").val();
+    var cost = $("#cost").val();
+    var pace = $("#pace").val();
+    var platform = $("#platform").val();
+    var font-back = $("#front-back").val();
+
+
+
 
     event.preventDefault();
     alert("test");
