d821df98cadfd72dfe298d4d052e78846eecad63
Grady L Shelton <gradyish@gmail.com>
2017-03-24 10:25:14 -0700

    write logic branching. if unable to afford cost, don't advise track. any mobile interest gets java. and non-mobile, front end interest gets css design. any established, nonmobile, back end interest gets c sharp. all that is left is ruby which is evolving, internal, back end design

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,56 @@ $(document).ready(function(){
     $("#form-wrapper").hide();
 
 
+
+    if (cost === "no") {
+      $("#wait").show();
+    } else {
+
+      if (platform === "mobile") {
+        $("#java").show();
+      } else if (pace === "established" && platform === "internal" && frontback === "back") {
+        $("#C").show();
+      } else if (platform === "internal" && frontback === "front") {
+        $("#design").show();
+      } else {
+        $("#ruby").show();
+      }
+    
+      // } else if (pace === "evolving" && platform === "mobile") {
+      //   $("#java").show();
+      // }
+
+
+
+
+
+    };
+
+    //   // show specific result
+    //   if (entertainment === "music") {
+    //     $("#old-music").show();
+    //   } else if (entertainment === "books") {
+    //     $("#old-books").show();
+    //   } else {
+    //     $("#old-movies").show();
+    //   }
+    //
+    // } else {
+    //   // if user is less than 40, check prefered entertainment
+    //   // show specific result
+    //   if (entertainment === "music") {
+    //     $("#young-music").show();
+    //   } else if (entertainment === "books") {
+    //     $("#young-books").show();
+    //   } else {
+    //     $("#young-movies").show();
+    //   }
+    //
+    // };
+
+
     alert("test");
+
   });
 
 
